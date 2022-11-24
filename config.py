f5c3a572d59b85f1b3b09f1e9a11552af08711aa
retroxhowdi <ssh-off@ya.ru>
2022-11-24 21:14:01 +0500

    first steps

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '5531596164:AAHIEJGJA45TWZ0GrgINb2Kl562quuj029E'
+text_menu = 'текст'
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import telebot
+import config
+import database
 
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler()
+def reply_message(message):
+    bot.reply_to(message, 'Используйте кнопки или команды.')
+
+@bot.message_handler(commands=['start'])
+def menu(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    bot.send_photo(message.chat.id, 'for_test.png', config.text_menu)
+
+
+
+
+bot.infinity_polling()
