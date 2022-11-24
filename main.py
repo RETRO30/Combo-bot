ad6c08b5a4feafea72f47c94634a205d9bc9af99
retroxhowdi <ssh-off@ya.ru>
2022-11-24 22:58:56 +0500

    second steps :)

--- main.py
+++ main.py
@@ -4,16 +4,45 @@ import database
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler()
-def reply_message(message):
-    bot.reply_to(message, 'Используйте кнопки или команды.')
+
+def get_buttons(command):
+    name_to_command = {'Главное меню': 'start',
+                       'Кнопка 1': 'test',
+                       'Кнопка 2': 'test',
+                       'Кнопка 3': 'test'}
+    buttons_for_command = {'start': ['Кнопка 1', 'Кнопка 2', 'Кнопка 3'],
+                           'test': ['Главное меню']}
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)
+    res = []
+    for button_name in buttons_for_command[command.replace('/', '')]:
+        res.append(telebot.types.InlineKeyboardButton(button_name, callback_data=name_to_command[button_name]))
+    keyboard.add(*res)
+    return keyboard
+
 
 @bot.message_handler(commands=['start'])
 def menu(message):
     bot.delete_message(message.chat.id, message.message_id)
-    bot.send_photo(message.chat.id, 'for_test.png', config.text_menu)
+    keyboard = get_buttons(message.text)
+    bot.send_photo(message.chat.id, telebot.types.InputFile('for_test.png'), config.text_menu, reply_markup=keyboard)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data == 'test':
+            keyboard = get_buttons(call.data)
+            bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id, caption="Тест",
+                                     reply_markup=keyboard)
+        if call.data == 'start':
+            keyboard = get_buttons(call.data)
+            bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                     caption=config.text_menu, reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: True)
+def reply_message(message):
+    bot.reply_to(message, 'Используйте кнопки или команды.')
 
 
 bot.infinity_polling()
