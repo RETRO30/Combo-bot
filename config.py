0c1d931737b62a22ba5c13e22f84ea3c595cbe2a
retroxhowdi <ssh-off@ya.ru>
2022-11-26 12:50:23 +0500

    second steps :)

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = '5531596164:AAHIEJGJA45TWZ0GrgINb2Kl562quuj029E'
 text_menu = 'текст'
+support_text = 'По вопросам технической поддержки пишите - @threesixcombo'
--- main.py
+++ main.py
@@ -7,11 +7,13 @@ bot = telebot.TeleBot(config.token)
 
 def get_buttons(command):
     name_to_command = {'Главное меню': 'start',
-                       'Кнопка 1': 'test',
-                       'Кнопка 2': 'test',
-                       'Кнопка 3': 'test'}
-    buttons_for_command = {'start': ['Кнопка 1', 'Кнопка 2', 'Кнопка 3'],
-                           'test': ['Главное меню']}
+                       'Заказы': 'test',
+                       'Отзывы': 'test',
+                       'Баланс': 'test',
+                       'Тех-поддержка': 'support'}
+    buttons_for_command = {'start': ['Заказы', 'Отзывы', 'Баланс', 'Тех-поддержка'],
+                           'test': ['Главное меню'],
+                           'support': ['Главное меню']}
     keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)
     res = []
     for button_name in buttons_for_command[command.replace('/', '')]:
@@ -38,11 +40,16 @@ def callback_inline(call):
             keyboard = get_buttons(call.data)
             bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                      caption=config.text_menu, reply_markup=keyboard)
+        if call.data == 'support':
+            keyboard = get_buttons(call.data)
+            bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                     caption=config.support_text, reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: True)
 def reply_message(message):
-    bot.reply_to(message, 'Используйте кнопки или команды.')
+    if message.chat.type == 'private':
+        bot.reply_to(message, 'Используйте кнопки или команды.')
 
 
 bot.infinity_polling()
