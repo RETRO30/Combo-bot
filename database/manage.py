e9a7c792da0e6e78fe983b7a287875cfd321b83c
Igorello74 <sidneff.igor@ya.ru>
2022-11-27 13:12:00 +0500

    fix database import problems

--- database/manage.py
+++ database/manage.py
@@ -4,6 +4,11 @@ import sys
 
 
 if __name__ == "__main__":
+    # A crutch to avoid import issues
+    if os.path.abspath('..') not in sys.path:
+        sys.path.append(os.path.abspath('..')) 
+
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     from django.core.management import execute_from_command_line
 
--- database/public.py
+++ database/public.py
@@ -1,21 +1,18 @@
-############################################################################
-## Django ORM Standalone Python Template
-############################################################################
-""" Here we'll import the parts of Django we need. """
+import os
+import sys
+
+import django
+
+# A crutch to avoid import issues
+if os.path.abspath('..') not in sys.path:
+    sys.path.append(os.path.abspath('..')) 
 
 # Turn off bytecode generation
-import sys
 sys.dont_write_bytecode = True
 
 # Django specific settings
-import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'database.settings')
-import django
 django.setup()
 
 # Import your models for use in your script
 from database.db.models import *
-
-############################################################################
-## START OF APPLICATION
-############################################################################
