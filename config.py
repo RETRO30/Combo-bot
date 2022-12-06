77ff98f181e781d4debac8c1e09b588259fe2d72
retroxhowdi <ssh-off@ya.ru>
2022-12-07 00:57:44 +0500

    поправил ошибки в текстах

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-token = '5801310763:AAHWmkgveSkzhrtAdmBwI3NrVWVlRJfeDo0'
+token = '5801310763:AAHTtS1KjndCpB_Vk7jFipm8SDOnzrIga2Y'
 
 REVIEWS_PER_A_DAY = 2 # максимальное количество отзывов с одного аккаунта за сутки
 FUCK_UP_TIME = timedelta(minutes=15) # время, после которого отзыв считается проебанным
